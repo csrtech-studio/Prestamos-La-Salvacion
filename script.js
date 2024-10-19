2667bff9814be42eed945877a202a56817b37da4
Csrhdz <105759540+Csrhdz@users.noreply.github.com>
2024-10-19 20:45:56 +0000

    arreglo de prestamo

--- script.js
+++ script.js
@@ -78,7 +78,6 @@ function displayLoans() {
         row.innerHTML = `
             <td><a href="#" onclick="confirmDelete(${index})">${loan.name}</a></td>
             <td>${loan.loanDate}</td>
-            <td>${loan.amount}</td> <!-- Mostrar el teléfono -->
             <td>${loan.totalToPay.toFixed(2)}</td>
             <td>${loan.remaining.toFixed(2)}</td>
             <td>
