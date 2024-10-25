09c8c80e4936b5c1f75931945db9a2c75a6f8fcd
Csrhdz <105759540+Csrhdz@users.noreply.github.com>
2024-10-25 21:03:11 +0000

    actualizar

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/serviceWorker.js')
+        navigator.serviceWorker.register('/Prestamos-La-Salvacion/serviceWorker.js')
             .then(registration => {
                 console.log('ServiceWorker registered successfully: ', registration);
             })
